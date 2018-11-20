22a8a965ec5a4e14b803944ed0b47f66092ffba2
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-20 00:08:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let currentUser = "Mike";
 
 let welcomeMessage = "Welcome to Flatbook, " + currentUser +"!";
 
-let excitedWelcomeMessage = "Welcome to Flatbook, ";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
 
 
 
